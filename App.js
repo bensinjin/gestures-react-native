13171f8b4a695f7d16aaf11808b770504660459d
Ben St John <ben@affinitybridge.com>
2017-11-23 10:11:58 -0800

    React demo: Adding descriptions, centering text.

--- App.js
+++ App.js
@@ -10,9 +10,10 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       currentZoom: settings.defaultZoom,
-      imageUri: settings.imageUri1
+      imageUri: settings.imageUri1,
+      description: settings.image1Description
     };
-    this.lastTapTimeStamp = this._getTs();
+    this.lastTapTS = null;
   }
   // This method is invoked only once, before rendering occurs for the first time.
   // At this point, there is still no native UI rendered for this element.
@@ -41,16 +42,16 @@ export default class App extends React.Component {
         // If we have no velocity along the x or y axis register this as a tap.
         // We don't want the app to accept a swipe as a tap although technically this is
         // fired on release so it's not entirely clear how velocity is determined
-        // (although I have observed it on release of swipes).
+        // (although I have observed it more noticeably on release of swipes).
         if (gestureState.vx == 0 && gestureState.vy == 0) {
-          // Set the current timestamp in ms.
-          let currentTS = this._getTs();
-          // If the current timestamp - the last timestamp is less than by default 500ms
+          let currentTapTS = this._getTs(),
+              lastTapTS = this.lastTapTS;
+          // If the current tap timestamp - the last tap timestamp is less than by default 500ms
           // we'll consider this a 'double tap'. Go ahead and reset the zoom level.
-          if (currentTS - this.lastTapTimeStamp <= settings.tapMillis) {
+          if (lastTapTS && currentTapTS - lastTapTS <= settings.tapMillis) {
             this._zoomReset();
           }
-          this.lastTapTimeStamp = currentTS;
+          this.lastTapTS = currentTapTS;
         }
       },
     });
@@ -70,6 +71,7 @@ export default class App extends React.Component {
           Weird Fruit
         </Text>
         <Image source={img} style={imgStyle}/>
+        <Text style={styles.textDescription}>{this.state.description}</Text>
         <Text style={styles.text}>
           Zoom level: {this.state.currentZoom}{'\n'}
           Drag left or right to zoom.{'\n'}
@@ -115,9 +117,12 @@ export default class App extends React.Component {
   // Select the next image.
   _nextPicture() {
     this.setState(previousState => {
-      let uri = previousState.imageUri == settings.imageUri1 ? settings.imageUri2 : settings.imageUri1;
+      let uri = previousState.imageUri == settings.imageUri1 ? settings.imageUri2 : settings.imageUri1,
+          description = previousState.description == settings.image1Description ? settings.image2Description : settings.image1Description;
+
       return {
-        imageUri: uri
+        imageUri: uri,
+        description: description
       }
     });
   }
@@ -130,17 +135,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 10,
   },
   titleText: {
     fontSize: 35,
     fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  textDescription: {
+    fontSize: 13,
+    fontWeight: 'bold',
+    margin: 30,
+    textAlign: 'center',
   },
   text: {
     fontSize: 15,
+    textAlign: 'center',
   },
   nextText: {
     fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
   }
 });
 
@@ -152,5 +167,7 @@ const settings = {
   minZoom: 5,
   tapMillis: 500,
   imageUri1: "http://wdy.h-cdn.co/assets/cm/15/09/768x516/54ebb801e0bf8_-_6-kiwano-xl.jpg",
-  imageUri2: "https://i.pinimg.com/736x/65/25/c9/6525c9b73eeb9527a8eb326e4e4fbe3b--weird-fruit-strange-fruit.jpg"
+  image1Description: "Cucumis metuliferus: Its fruit has horn-like spines, hence the name \"horned melon\". Ripe fruit has orange skin and lime green, jelly-like flesh with a refreshingly fruity taste, and texture similar to a passionfruit or pomegranate.",
+  imageUri2: "https://i.pinimg.com/736x/65/25/c9/6525c9b73eeb9527a8eb326e4e4fbe3b--weird-fruit-strange-fruit.jpg",
+  image2Description: "Durian: There are 30 recognised Durio species, at least nine of which produce edible fruit, and over 300 named varieties in Thailand and 100 in Malaysia.[3] Durio zibethinus is the only species available in the international market: other species are sold in their local regions.",
 }
